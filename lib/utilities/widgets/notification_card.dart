a6825c313a865e192363f5ecfcd06fd3cf33701a
Ali Akbar <akbarstudyinfo786@gmail.com>
2024-03-06 17:12:19 +0500

    Completed changes booking feature

--- lib/utilities/widgets/notification_card.dart
+++ lib/utilities/widgets/notification_card.dart
@@ -42,108 +42,111 @@ class _NotificationCardState extends State<NotificationCard> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(30)),
-                  child: CustomNetworkImage(
-                    imageUrl: "",
-                    width: 30,
-                    height: 30,
-                  ),
-                ),
-                gapW8,
-                Expanded(
-                  child: Column(
+            FutureBuilder<UserModel?>(
+                future: UserRepo()
+                    .fetchUser(profileId: widget.booking.rentalUserId),
+                builder: (context, snapshot) {
+                  UserModel? user;
+                  if (snapshot.hasData) {
+                    user = snapshot.data;
+                  }
+                  return Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Text(
-                        "---",
-                        style: TextStyle(
-                          fontFamily: Assets.plusJakartaFont,
-                          color: StyleGuide.textColor2,
-                          fontSize: 12,
-                          fontWeight: FontWeight.w700,
+                      ClipRRect(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(30)),
+                        child: CustomNetworkImage(
+                          imageUrl: user?.imageUrl ?? "",
+                          width: 30,
+                          height: 30,
                         ),
                       ),
-                      gapH4,
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Flexible(
-                            child: Text(
-                              "I want to book your ${widget.booking.car} car.",
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
+                      gapW8,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              user?.firstName ?? "---",
                               style: const TextStyle(
                                 fontFamily: Assets.plusJakartaFont,
-                                fontSize: 9,
-                                fontWeight: FontWeight.w400,
+                                color: StyleGuide.textColor2,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700,
                               ),
                             ),
-                          ),
-                          gapW14,
-                          Image.asset(
-                            "assets/icons/car.png",
-                            height: 23,
-                            width: 48,
-                            fit: BoxFit.fill,
-                          ),
-                          gapW10,
+                            gapH4,
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Flexible(
+                                  child: Text(
+                                    "I want to book your ${widget.booking.car} car.",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      fontFamily: Assets.plusJakartaFont,
+                                      fontSize: 9,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ),
+                                gapW14,
+                                Image.asset(
+                                  "assets/icons/car.png",
+                                  height: 23,
+                                  width: 48,
+                                  fit: BoxFit.fill,
+                                ),
+                                gapW10,
 
-                          /// Remaining Time Widgets
-                          BlocSelector<DataFetcherCubit, dynamic,
-                              DataFetcherTimePeriod?>(
-                            selector: (state) => state is DataFetcherTimePeriod
-                                ? DataFetcherTimePeriod()
-                                : null,
-                            builder: (context, state) {
-                              return Text(
-                                parseTimePeriod(
-                                    atTime: widget.booking.createdAt),
-                                style: const TextStyle(
-                                    fontFamily: Assets.plusJakartaFont,
-                                    fontSize: 8,
-                                    fontWeight: FontWeight.w400,
-                                    color: StyleGuide.textColor2),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                      Text(
-                        DateFormat("dd/MM/yyyy")
-                            .format(widget.booking.bookingDate),
-                        style: const TextStyle(
-                          color: StyleGuide.textColor2,
-                          fontFamily: Assets.plusJakartaFont,
-                          fontSize: 9,
-                          fontWeight: FontWeight.w500,
+                                /// Remaining Time Widgets
+                                BlocSelector<DataFetcherCubit, dynamic,
+                                    DataFetcherTimePeriod?>(
+                                  selector: (state) =>
+                                      state is DataFetcherTimePeriod
+                                          ? DataFetcherTimePeriod()
+                                          : null,
+                                  builder: (context, state) {
+                                    return Text(
+                                      parseTimePeriod(
+                                          atTime: widget.booking.createdAt),
+                                      style: const TextStyle(
+                                          fontFamily: Assets.plusJakartaFont,
+                                          fontSize: 8,
+                                          fontWeight: FontWeight.w400,
+                                          color: StyleGuide.textColor2),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                            Text(
+                              DateFormat("dd/MM/yyyy")
+                                  .format(widget.booking.bookingDate),
+                              style: const TextStyle(
+                                color: StyleGuide.textColor2,
+                                fontFamily: Assets.plusJakartaFont,
+                                fontSize: 9,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            gapH4,
+                            Text(
+                              remainingTime(atTime: widget.booking.bookingDate),
+                              style: const TextStyle(
+                                  fontFamily: Assets.plusJakartaFont,
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w700,
+                                  color: StyleGuide.primaryColor),
+                            ),
+                          ],
                         ),
                       ),
-                      gapH4,
-                      BlocSelector<DataFetcherCubit, dynamic,
-                          DataFetcherTimePeriod?>(
-                        selector: (state) => state is DataFetcherTimePeriod
-                            ? DataFetcherTimePeriod()
-                            : null,
-                        builder: (context, state) {
-                          return Text(
-                            remainingTime(atTime: widget.booking.bookingDate),
-                            style: const TextStyle(
-                                fontFamily: Assets.plusJakartaFont,
-                                fontSize: 11,
-                                fontWeight: FontWeight.w700,
-                                color: StyleGuide.primaryColor),
-                          );
-                        },
-                      ),
                     ],
-                  ),
-                ),
-              ],
-            ),
+                  );
+                }),
             gapH16,
             Center(
               child: isRentalUser
